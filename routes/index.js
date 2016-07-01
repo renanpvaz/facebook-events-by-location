25e4cf8807de6e00379f36bb5b77e5e093d8d8a1
Renan Vaz <renanp.vaz@outlook.com>
2016-07-01 01:01:08 -0300

    refactor index.js

--- routes/index.js
+++ routes/index.js
@@ -1,198 +1,241 @@
-var express = require('express');
-var router = express.Router();
-var Promise = require("bluebird");
-var rp = require('request-promise');
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const Promise = require('bluebird');
+const rp = require('request-promise');
+
+const baseUrl = 'https://graph.facebook.com/v2.5/';
+const fieldsQs = 'id,name,cover.fields(id,source),picture.type(large),location,events.fields(id,name,cover.fields(id,source),picture.type(large),description,start_time,attending_count,declined_count,maybe_count,noreply_count).since(';
 
 function calculateStarttimeDifference(currentTime, dataString) {
-  return (new Date(dataString).getTime()-(currentTime*1000))/1000;
+  return (new Date(dataString).getTime() - (currentTime * 1000 )) / 1000;
 }
 
-function compareVenue(a,b) {
-  if (a.venueName < b.venueName)
-    return -1;
-  if (a.venueName > b.venueName)
-    return 1;
-  return 0;
+function compareVenue(a, b) {
+  return compare(a, b, 'venueName');
 }
 
-function compareTimeFromNow(a,b) {
-  if (a.eventTimeFromNow < b.eventTimeFromNow)
-    return -1;
-  if (a.eventTimeFromNow > b.eventTimeFromNow)
-    return 1;
-  return 0;
+function compareTimeFromNow(a, b) {
+  return compare(a, b, 'eventTimeFromNow');
 }
 
-function compareDistance(a,b) {
-  var aEventDistInt = parseInt(a.eventDistance, 10);
-  var bEventDistInt = parseInt(b.eventDistance, 10);
-  if (aEventDistInt < bEventDistInt)
+function compareDistance(a, b) {
+  a.eventDistance = parseInt(a.eventDistance, 10);
+  b.eventDistance = parseInt(b.eventDistance, 10);
+
+  return compare(a, b, 'eventDistance');
+}
+
+function compare(valA, valB, property) {
+  if (valA[property] < valB[property]) {
     return -1;
-  if (aEventDistInt > bEventDistInt)
+  }
+  else if (valA[property] > valB[property]) {
     return 1;
+  }
+
   return 0;
 }
 
 function comparePopularity(a,b) {
-  if ((a.eventStats.attendingCount + (a.eventStats.maybeCount / 2)) < (b.eventStats.attendingCount + (b.eventStats.maybeCount / 2)))
+  let aStats = a.eventStats;
+  let bStats = b.eventStats;
+
+  if ((aStats.attendingCount + (aStats.maybeCount / 2)) < (bStats.attendingCount + (bStats.maybeCount / 2)))
     return 1;
-  if ((a.eventStats.attendingCount + (a.eventStats.maybeCount / 2)) > (b.eventStats.attendingCount + (b.eventStats.maybeCount / 2)))
+  if ((aStats.attendingCount + (aStats.maybeCount / 2)) > (bStats.attendingCount + (bStats.maybeCount / 2)))
     return -1;
   return 0;
 }
 
-function haversineDistance(coords1, coords2, isMiles) {
+function haversineDistance(coordsA, coordsB, isMiles) {
+  const R = 6371;
 
-  //coordinate is [latitude, longitude]
+  let latA = coordsA[0];
+  let lonA = coordsA[1];
+
+  let latB = coordsB[0];
+  let lonB = coordsB[1];
+
   function toRad(x) {
     return x * Math.PI / 180;
   }
 
-  var lon1 = coords1[1];
-  var lat1 = coords1[0];
-
-  var lon2 = coords2[1];
-  var lat2 = coords2[0];
-
-  var R = 6371; // km
-
-  var x1 = lat2 - lat1;
-  var dLat = toRad(x1);
-  var x2 = lon2 - lon1;
-  var dLon = toRad(x2);
-  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+  let x1 = latB - latA;
+  let dLat = toRad(x1);
+  let x2 = lonB - lonA;
+  let dLon = toRad(x2);
+  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(toRad(latA)) * Math.cos(toRad(latB)) *
       Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
 
-  if(isMiles) d /= 1.60934;
+  if(!!isMiles) {
+    d /= 1.60934;
+  }
 
   return d;
 }
 
-router.get('/', function(req, res, next) {
-  res.json({ "message": "Welcome to the Facebook Event Search service!" });
+router.get('/', function(req, res) {
+  res.json({ 'message': 'Welcome to the Facebook Event Search service!' });
 });
 
-router.get('/events', function(req, res, next) {
+router.get('/events', function(req, res) {
+  let query = req.query;
 
-  if (!req.query.lat || !req.query.lng || !req.query.distance || !req.query.access_token) {
-    res.status(500).send({error: "Please specify the lat, lng, distance and access_token query parameters"});
-  } else {
+  if (!query.lat || !query.lng || !query.distance || !query.access_token) {
+    res.status(500).send({
+      error: 'Please specify the lat, lng, distance and access_token query parameters'
+    });
+  }
+  else {
+    const idLimit = 50;
+    let currentTimestamp = (new Date().getTime() / 1000).toFixed();
+    let venuesCount = 0, venuesWithEvents = 0, eventsCount = 0;
 
-    var idLimit = 50, //FB only allows 50 ids per /?ids= call
-        currentTimestamp = (new Date().getTime()/1000).toFixed(),
-        venuesCount = 0,
-        venuesWithEvents = 0,
-        eventsCount = 0,
-        placeUrl = "https://graph.facebook.com/v2.5/search?type=place&q=&center=" + req.query.lat + "," + req.query.lng + "&distance=" + req.query.distance + "&limit=1000&fields=id&access_token=" + req.query.access_token;
+    const options = {
+        uri: `${baseUrl}/search`,
+        qs: {
+            access_token: query.access_token,
+            type: 'place',
+            center: `${query.lat}, ${query.lng}`,
+            distance: query.distance,
+            limit: 1000,
+            fields: 'id'
+        },
+        json: true
+    };
 
-    //Get places as specified
-    rp.get(placeUrl).then(function(responseBody) {
+    rp.get(options).then(
+      (responseBody) => {
+        let ids = [];
+        let tempArray = [];
+        let data = responseBody.data;
 
-      var ids = [],
-          tempArray = [],
-          data = JSON.parse(responseBody).data;
+        venuesCount = data.length;
 
-      //Set venueCount
-      venuesCount = data.length;
+        try {
+          data.forEach(function(idObj, index, arr) {
+            tempArray.push(idObj.id);
 
-      //Create array of 50 places each
-      data.forEach(function(idObj, index, arr) {
-        tempArray.push(idObj.id);
-        if (tempArray.length >= idLimit) {
-          ids.push(tempArray);
-          tempArray = [];
+            if (tempArray.length >= idLimit) {
+              ids.push(tempArray);
+              tempArray = [];
+            }
+          });
+
+          if (tempArray.length > 0) {
+            ids.push(tempArray);
+          }
+        } catch (e) {
+          console.error(e);
         }
-      });
 
-      // Push the remaining places
-      if (tempArray.length > 0) {
-        ids.push(tempArray);
+        return ids;
+      }
+    ).then((ids) => {
+      let promises = [];
+
+      try {
+        ids.forEach(function(idArray, index, arr) {
+          let options = {
+              uri: `${baseUrl}`,
+              qs: {
+                  access_token: query.access_token,
+                  ids: idArray.join(','),
+                  fields: fieldsQs + currentTimestamp + ')'
+              }
+          };
+
+          promises.push(rp.get(options));
+        });
+      } catch (e) {
+        console.error(e);
       }
 
-      return ids;
-    }).then(function(ids) {
+      return promises;
 
-      var urls = [];
-
-      //Create a Graph API request array (promisified)
-      ids.forEach(function(idArray, index, arr) {
-        urls.push(rp.get("https://graph.facebook.com/v2.5/?ids=" + idArray.join(",") + "&fields=id,name,cover.fields(id,source),picture.type(large),location,events.fields(id,name,cover.fields(id,source),picture.type(large),description,start_time,attending_count,declined_count,maybe_count,noreply_count).since(" + currentTimestamp + ")&access_token=" + req.query.access_token));
-      });
-
-      return urls;
-
-    }).then(function(promisifiedRequests) {
-
-      //Run Graph API requests in parallel
+    }).then((promisifiedRequests) => {
       return Promise.all(promisifiedRequests)
-
     })
-    .then(function(results){
+    .then((results) => {
+      let sort = query.sort.toLowerCase();
+      let events = [];
 
-      var events = [];
-
-      //Handle results
-      results.forEach(function(resStr, index, arr) {
+      results.forEach((resStr, index, arr) => {
         var resObj = JSON.parse(resStr);
-        Object.getOwnPropertyNames(resObj).forEach(function(venueId, index, array) {
-          var venue = resObj[venueId];
-          if (venue.events && venue.events.data.length > 0) {
-            venuesWithEvents++;
-            venue.events.data.forEach(function(event, index, array) {
-              var eventResultObj = {};
-              eventResultObj.venueId = venueId;
-              eventResultObj.venueName = venue.name;
-              eventResultObj.venueCoverPicture = (venue.cover ? venue.cover.source : null);
-              eventResultObj.venueProfilePicture = (venue.picture ? venue.picture.data.url : null);
-              eventResultObj.venueLocation = (venue.location ? venue.location : null);
-              eventResultObj.eventId = event.id;
-              eventResultObj.eventName = event.name;
-              eventResultObj.eventCoverPicture = (event.cover ? event.cover.source : null);
-              eventResultObj.eventProfilePicture = (event.picture ? event.picture.data.url : null);
-              eventResultObj.eventDescription = (event.description ? event.description : null);
-              eventResultObj.eventStarttime = (event.start_time ? event.start_time : null);
-              eventResultObj.eventDistance = (venue.location ? (haversineDistance([venue.location.latitude, venue.location.longitude], [req.query.lat, req.query.lng], false)*1000).toFixed() : null);
-              eventResultObj.eventTimeFromNow = calculateStarttimeDifference(currentTimestamp, event.start_time);
-              eventResultObj.eventStats = {
-                attendingCount: event.attending_count,
-                declinedCount: event.declined_count,
-                maybeCount: event.maybe_count,
-                noreplyCount: event.noreply_count
-              };
-              events.push(eventResultObj);
-              eventsCount++;
-            });
+
+        Object.getOwnPropertyNames(resObj).forEach((venueId, index, array) => {
+          let venue = resObj[venueId];
+
+          try {
+            if (venue.events && venue.events.data.length > 0) {
+              venuesWithEvents++;
+
+              venue.events.data.forEach(function(event, index, array) {
+                let result = {
+                  venueId,
+                  venueName: venue.name,
+                  venueCoverPicture: (venue.cover ? venue.cover.source : null),
+                  venueProfilePicture: (venue.picture ? venue.picture.data.url : null),
+                  venueLocation: (venue.location ? venue.location : null),
+                  eventId: event.id,
+                  eventName: event.name,
+                  eventCoverPicture: (event.cover ? event.cover.source : null),
+                  eventProfilePicture: (event.picture ? event.picture.data.url : null),
+                  eventDescription: (event.description ? event.description : null),
+                  eventStarttime: (event.start_time ? event.start_time : null),
+                  eventDistance: (venue.location ? (haversineDistance([venue.location.latitude, venue.location.longitude], [req.query.lat, req.query.lng], false)*1000).toFixed() : null),
+                  eventTimeFromNow: calculateStarttimeDifference(currentTimestamp, event.start_time),
+                  eventStats: {
+                    attendingCount: event.attending_count,
+                    declinedCount: event.declined_count,
+                    maybeCount: event.maybe_count,
+                    noreplyCount: event.noreply_count
+                  }
+                };
+
+                events.push(result);
+                eventsCount++;
+              });
+            }
+          } catch (e) {
+            console.error(e);
           }
         });
       });
 
-      //Sort if requested
-      if (req.query.sort && (req.query.sort.toLowerCase() === "time" || req.query.sort.toLowerCase() === "distance" || req.query.sort.toLowerCase() === "venue" || req.query.sort.toLowerCase() === "popularity")) {
-        if (req.query.sort.toLowerCase() === "time") {
+      if (!!req.query.sort) {
+        if (sort === 'time') {
           events.sort(compareTimeFromNow);
         }
-        if (req.query.sort.toLowerCase() === "distance") {
+        if (sort === 'distance') {
           events.sort(compareDistance);
         }
-        if (req.query.sort.toLowerCase() === "venue") {
+        if (sort === 'venue') {
           events.sort(compareVenue);
         }
-        if (req.query.sort.toLowerCase() === "popularity") {
+        if (sort === 'popularity') {
           events.sort(comparePopularity);
         }
       }
 
-      //Produce result object
-      res.send({events: events, metadata: {venues: venuesCount, venuesWithEvents: venuesWithEvents, events: eventsCount}});
-
-    }).catch(function (e) {
+      res.send({
+        events,
+        metadata: {
+          venues: venuesCount,
+          venuesWithEvents,
+          events: eventsCount
+        }
+      });
+    }).catch((e) => {
+      console.log(e);
       res.status(500).send({error: e});
     });
   }
-
 });
 
 module.exports = router;
